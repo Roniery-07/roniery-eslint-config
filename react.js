3339e7fe6bc4c68614c7ecfaf69a57b902c5c018
Roniery Abreu <heroniery007@gmail.com>
2025-11-17 11:55:40 -0300

    adds prettier

--- react.js
+++ react.js
@@ -4,9 +4,11 @@ import tseslint from 'typescript-eslint';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist', 'build', 'coverage'] }, // Pastas a ignorar globalmente
+  { ignores: ['dist', 'build', 'coverage'] },
 
   js.configs.recommended,
   ...tseslint.configs.recommended,
@@ -17,36 +19,38 @@ export default tseslint.config(
       react: reactPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettierPlugin, // Adiciona o plugin do Prettier
     },
     languageOptions: {
       ecmaVersion: 2022,
-      globals: {
-        ...globals.browser,
-        ...globals.es2021
-      },
+      globals: { ...globals.browser, ...globals.es2021 },
       parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
+        ecmaFeatures: { jsx: true },
       },
     },
     settings: {
-      react: {
-        version: 'detect',
-      },
+      react: { version: 'detect' },
     },
     rules: {
       ...reactPlugin.configs.recommended.rules,
       ...reactPlugin.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,      
+          semi: true,             
+          trailingComma: 'all',   
+          printWidth: 80,         
+          tabWidth: 2,            
+          arrowParens: 'always',  
+          endOfLine: 'auto',
+        },
       ],
-
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'react/prop-types': 'off',
     },
-  }
+  },
+
+  prettierConfig,
 );
